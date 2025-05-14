5c038947d9bc958db7b184a23ab4a5b871f9e24c
Bryan Palmer <palmer.bryan@gmail.com>
2025-05-14 12:37:21 +1000

    update __init__.py for line_plot.py

--- src/mgplot/__init__.py
+++ src/mgplot/__init__.py
@@ -6,12 +6,19 @@ __version__ = "0.0.1"
 __author__ = "Bryan Palmer"
 __all__ = (
     "finalise_plot",
+    "get_finalise_kwargs_list",
     "get_setting",
     "set_setting",
+    "set_chart_dir",
+    "clear_chart_dir",
+    "line_plot",
+    "line_plot_finalise",
+    "line_plot_multistart",
 )
 # __pdoc__: dict[str, Any] = {}  # hide submodules from documentation
 
 
 # --- local imports
-from mgplot.finalise_plot import finalise_plot
-from mgplot.settings import get_setting, set_setting
+from mgplot.finalise_plot import finalise_plot, get_finalise_kwargs_list
+from mgplot.settings import get_setting, set_setting, set_chart_dir, clear_chart_dir
+from mgplot.line_plot import line_plot, line_plot_finalise, line_plot_multistart
